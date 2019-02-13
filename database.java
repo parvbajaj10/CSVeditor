de196abe44251c0f053f14cf4e2febf227bf028c
parvbajaj10 <46554143+parvbajaj10@users.noreply.github.com>
2019-02-13 13:45:16 +0530

    Update and rename new1.java to database.java

--- database.java
+++ database.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-public class new1
+public class database
 {
    public static void main(String []args)
    {
@@ -10,28 +10,28 @@ public class new1
        int ch,k;
        while(true)
        {
-           System.out.println("01. insert rows:");
-           System.out.println("02. delete rows");
-           System.out.println("03. select from the table");
-	   System.out.println("04. show table in database");
+           System.out.println("01. insert rows:"); // This option will ins
+           System.out.println("02. delete rows"); // This option will delete rows
+           System.out.println("03. select from the table"); // This option will display the table which we want to show
+	   System.out.println("04. show table in database"); // This option will tell us about the tables in the database
            System.out.print("Enter Your Choice : ");
            ch=sc.nextInt();
            switch(ch)
            {
                case 1:
-                        System.out.print("enter the row you want to insert:");
+                        System.out.print("enter the row you want to insert:"); //Enterting the details here
 			 
     String csvfilepath = "C:\\Users\\bajaj\\Desktop\\project\\csvs";
      try (PrintWriter writer = new PrintWriter(new File("example.csv"))) 
     {
 
       StringBuilder sb = new StringBuilder();
-      sb.append("id,");
+      sb.append("id,"); // Serial No
       sb.append(',');
-      sb.append("Name");
+      sb.append("Name");// Name of the person
       sb.append('\n');
 
-      sb.append("1");
+      sb.append("1"); // Prefed data
       sb.append(',');
       sb.append("Prashant Ghimire");
       sb.append('\n');
@@ -50,18 +50,18 @@ public class new1
 
                   
                     case 2:  
-                        System.out.print("enter the row you want to delete");
+                        System.out.print("enter the row you want to delete"); // Deleting the row we want to
                         break;
-               case 3: System.out.print("select from the table:");
+               case 3: System.out.print("select from the table:"); // showing the table we want to
 	
     break;
 
 
   
-		case 4: System.out.print("show table in database");
+		case 4: System.out.print("show table in database"); //It will be showing the table
 			break;
   
-               default: System.out.println("Wrong Entry");
+               default: System.out.println("Wrong Entry"); // The default statement that the program will be running
              }
            }
          }
